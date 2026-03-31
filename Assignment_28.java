db326d665a4b07f98808e959b3c12b181ec5e41d
nidhi-19876 <nidhisonkusre46@gmail.com>
2026-03-31 21:01:08 +0530

    Add files via upload

---
+++ Assignment_28.java
@@ -0,0 +1,8 @@
+// 8. Create a class TextProcessingBuilder.
+class TextProcessingBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" Builder");
+        System.out.println(sb);
+    }
+}
